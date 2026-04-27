c43324a50bba9d03cb2f0e44c78b2059f80a948f
hachikoo <mochamad.rendhi@gmail.com>
2026-04-27 13:03:37 +0700

    a

--- resources/views/master/sparepart/index.blade.php
+++ resources/views/master/sparepart/index.blade.php
@@ -979,12 +979,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-const BASE_URL = window.location.origin;
 
-function api(url, params = {}) {
-    const query = new URLSearchParams(params).toString();
-    return fetch(`${BASE_URL}${url}?${query}`);
-}
 
     </script>
 @endpush
